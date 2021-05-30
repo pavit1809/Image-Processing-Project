2f1e554e5cffdd656ec80465094e4018a16fdfaa
pavit1809 <pavitragoyal@hotmail.com>
2021-05-30 17:09:48 +0530

    Created and tested backend api for processing and web

---
+++ Web/Backend/app.js
@@ -0,0 +1,69 @@
+const express=require('express');
+const axios=require('axios').default;
+const cors=require('cors');
+
+const app=express();
+
+app.use(cors());
+
+app.use(express.json({ limit: "10mb", extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+// Some formatting in the incoming b64 string and also look for type
+
+
+//vars for temporary storing of response data of encrypt data from the api since db is not used
+let encimg,kr,kc,iter_max;
+
+const port=process.env.PORT || 8080;
+
+app.get('/',(req,res)=>{
+  res.send("hello");
+})
+
+app.post('/encrypt',async (req,res)=>{
+  try{    
+    const response=await axios.post('http://127.0.0.1:5000/encrypt',req.body);
+    // console.log(response);
+    encimg=response.data.img;
+    kr=response.data.kr
+    kc=response.data.kc
+    iter_max=response.data.iter_max
+    res.send({
+      status: 200,
+      success: true,
+      result: response.data,
+      message:"Api is running fine1"
+    });
+  }catch(err){
+    res.send({
+      status: 400,
+      success: false,
+      message:err.message,
+      result: null
+    });
+  }
+})
+
+app.post('/decrypt',async(req,res)=>{
+  try{
+    const response=await axios.post('http://127.0.0.1:5000/decrypt',req.body);
+    res.send({
+      status: 200,
+      success: true,
+      result: response.data,
+      message:"Api is running fine1"
+    });
+  }catch(err){
+    res.send({
+      status: 400,
+      success: false,
+      message:err.message,
+      result: null
+    });
+  }
+})
+
+app.listen(port,(req,res)=>{
+  console.log(`Server started @ ${port}`);
+})
